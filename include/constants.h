8af50a348577eff445256dbccba83f551638ccc5
Sergei2000 <dsdem@mail.ru>
2020-05-12 14:15:25 +0300

    initial

---
+++ include/constants.h
@@ -0,0 +1,9 @@
+//
+// Created by sergei on 06.05.2020.
+//
+
+#ifndef UNTITLED2_CONSTANTS_H
+#define UNTITLED2_CONSTANTS_H
+#define    Path1 "/mydb"
+#define    Path2 "/bd2"
+#endif //UNTITLED2_CONSTANTS_H
